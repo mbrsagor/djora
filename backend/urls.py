70836f43409fbbae9572199e6a9453f6213fe281
mbrsagor <mbrsagor@gmail.com>
2026-01-28 17:40:24 +0600

    feat: Add various static assets including plugins, CSS, images, and backend templates.

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from backend.views import add_slider_view
+from backend.views import add_slider_view, dashboard_view
 
 
 urlpatterns = [
+    path('dashboard/', dashboard_view.DashboardView.as_view(), name='dashboard'),
     path('add-new-slider/', add_slider_view.AddSliderView.as_view(), name='add_slider'),
 ]
