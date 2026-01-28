35b270b55b51db952383200f2f7bf9678d0441ce
mbrsagor <mbrsagor@gmail.com>
2026-01-28 19:42:49 +0600

    New: Implement full CRUD functionality for slider management, including new views, URLs, and backend sidebar navigation.

--- backend/urls.py
+++ backend/urls.py
@@ -4,5 +4,9 @@ from backend.views import add_slider_view, dashboard_view
 
 urlpatterns = [
     path('dashboard/', dashboard_view.DashboardView.as_view(), name='dashboard'),
+    # Slider
     path('add-slider/', add_slider_view.AddSliderView.as_view(), name='add_slider'),
+    path('slider-listview/', add_slider_view.SliderListView.as_view(), name='slider_listview'),
+    path('slider-update/<pk>/', add_slider_view.SliderUpdateView.as_view(), name='slider_update'),
+    path('slider-delete/<pk>/', add_slider_view.SliderDeleteView.as_view(), name='slider_delete'),
 ]
--- backend/views/add_slider_view.py
+++ backend/views/add_slider_view.py
@@ -9,10 +9,56 @@ from backend.forms.slider_form import SliderModelForm
 from backend.models import Slider
 
 
-@method_decorator(login_required(login_url='/core/signin/'), name='dispatch')
+@method_decorator(login_required(login_url='/user/signin/'), name='dispatch')
 class AddSliderView(SuccessMessageMixin, generic.CreateView):
     model = Slider
     form_class = SliderModelForm
     template_name = 'features/slider/add_slider.html'
     success_url = '/backend/add-slider/'
     success_message = 'Slider created successfully'
+
+
+
+@method_decorator(login_required(login_url='/user/signin/'), name='dispatch')
+class SliderListView(generic.ListView):
+    """
+    Slider listview with pagination
+    URL: /core/slider-listview/
+    Method: GET
+    :param
+    """
+    model = Slider
+    paginate_by = 8
+    context_object_name = 'sliders'
+    filterset_fields = ['title', 'subtitle']
+    template_name = 'features/slider/slider_listview.html'
+
+
+@method_decorator(login_required(login_url='/user/signin/'), name='dispatch')
+class SliderUpdateView(SuccessMessageMixin, generic.UpdateView):
+    """
+    Slider update view
+    URL: /core/slider-update/<pk>/
+    Method: PUT
+    :param
+    """
+    model = Slider
+    form_class = SliderModelForm
+    success_message = 'Slider has been updated'
+    template_name = 'features/slider/add_slider.html'
+
+    def get_success_url(self):
+        return reverse('slider_update', kwargs={'pk': self.object.id})
+
+
+@method_decorator(login_required(login_url='/user/signin/'), name='dispatch')
+class SliderDeleteView(SuccessMessageMixin, generic.DeleteView):
+    """
+    Slider Delete View
+    URL: /backend/slider-delete/<pk>/
+    Method: DELETE
+    """
+    model = Slider
+    success_url = '/backend/slider-listview/'
+    success_message = 'Slider has been deleted.'
+    template_name = 'common/delete_confirm.html'
